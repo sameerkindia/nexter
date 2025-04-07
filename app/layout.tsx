68b42f24b543b11924e249f972acb576d6f9c346
sameerkindia <sameerkhanaa5@gmail.com>
2025-04-07 09:22:10 +0530

    nexter project setup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Josefin_Sans } from "next/font/google";
+import "./globals.css";
+
+
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
+  subsets: ["latin"],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Nexter",
+  description: "Create with nextjs 15. with the help of grid layout",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefinSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
