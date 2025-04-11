e5c71202368bbe5d08ae4784f060cdd19b4a83cd
sameerkindia <sameerkhanaa5@gmail.com>
2025-04-11 09:13:52 +0530

    header and sidebar tailwind implimented

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="sidebar bg-primary col-span-1 row-span-full flex justify-center max-2md:col-span-full max-2md:row-span-1 max-2md:items-center max-2md:justify-end">
-      <button className="nav-btn border-none rounded-none bg-white h-0.5 w-[45px] mt-10 max-2md:mt-0 mr-[30px] "></button>
+      <button className="nav-btn border-none rounded-none bg-white h-0.5 w-[45px] !mt-10 max-2md:mt-0 !mr-[30px]"></button>
     </div>
   );
 };
