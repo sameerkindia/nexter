fc19ce9a3816066dceb982797dd41df09fe951a0
sameerkindia <sameerkhanaa5@gmail.com>
2025-04-15 09:28:05 +0530

    story

--- components/StoryPictures.tsx
+++ components/StoryPictures.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const StoryPictures = () => {
-  return <div className="story__pictures" style={{
-    backgroundImage: `linear-gradient(rgba(198, 153, 99, .5), rgba(198, 153, 99, .5)), url(./img/back.jpg)`
-  }}>
-  <img src="img/story-1.jpeg" alt="Couple with new house" className="story__img--1" />
-  <img src="img/story-2.jpeg" alt="New house"className="story__img--2" />
-</div>;
+  return <div></div>
 };
 
 export default StoryPictures;
